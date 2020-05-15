2db959df68d9c798af99fa94115fb2bc90e960e9
mustafaMagdy1111 <mustafa.magdy1111@gmail.com>
2020-05-15 21:57:27 +0200

    init frotn project

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -18,7 +18,7 @@ namespace Persistence
                 .HasData(
                     new Value {Id = 1, Name = "Value 101"},
                     new Value {Id = 2, Name = "Value 102"},
-                    new Value {Id = 3, Name = "Value 103"}
+                    new Value {Id = 3, Name = "Value 104"}
                 );
         }
     }
---
+++ Persistence/Migrations/20200514132545_intiralfsgd.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class intiralfsgd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Value 104");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Value 103");
+        }
+    }
+}
