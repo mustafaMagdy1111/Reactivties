fb3184ddc9d827222178055ac6988c4c1f4c33d1
mustafaMagdy1111 <mustafa.magdy1111@gmail.com>
2020-06-15 15:49:37 +0200

    Finish Axios

--- front-end/src/app/features/activities/Dashboard/ActivityDashboard.tsx
+++ front-end/src/app/features/activities/Dashboard/ActivityDashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import {Grid, List} from 'semantic-ui-react'
 import {IActivity} from './../../../models/activity';
 import ActivityDetails from '../details/ActivityDetails';
@@ -7,58 +7,64 @@ import ActivityForm from '../../form/ActivityForm';
 
 
 interface IProps {
-    activities: IActivity[];
-    selectActivity: (id: string) => void;
-    selectedActivity: IActivity | null;
-    editMode: boolean;
-    setEditMode: (editMode: boolean) => void;
-    setSelectedActivity: (activity: IActivity | null) => void;
-    createActivity: (activity: IActivity) => void;
-    editActivity: (activity: IActivity) => void;
-    deleteActivity: (id: string) => void;
-  }
-  
-  const ActivityDashboard: React.FC<IProps> = ({
-    activities,
-    selectActivity,
-    selectedActivity,
-    editMode,
-    setEditMode,
-    setSelectedActivity,
-    createActivity,
-    editActivity,
-    deleteActivity
-  }) => {
-    return (
-      <Grid>
-        <Grid.Column width={10}>
-          <ActivityList
-            activities={activities}
-            selectActivity={selectActivity}
-            deleteActivity={deleteActivity}
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null;
+  editMode: boolean;
+  setEditMode: (editMode: boolean) => void;
+  setSelectedActivity: (activity: IActivity | null) => void;
+  createActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
+  deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+  submitting: boolean;
+  target: string;
+}
+
+const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity,
+  editMode,
+  setEditMode,
+  setSelectedActivity,
+  createActivity,
+  editActivity,
+  deleteActivity,
+  submitting,
+  target
+}) => {
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
+          submitting={submitting}
+          target={target}
+        />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setEditMode={setEditMode}
+            setSelectedActivity={setSelectedActivity}
           />
-        </Grid.Column>
-        <Grid.Column width={6}>
-          {selectedActivity && !editMode && (
-            <ActivityDetails
-              activity={selectedActivity}
-              setEditMode={setEditMode}
-              setSelectedActivity={setSelectedActivity}
-            />
-          )}
-          {editMode && (
-            <ActivityForm
-              key={(selectedActivity && selectedActivity.id) || 0}
-              setEditMode={setEditMode}
-              activity={selectedActivity!}
-              createActivity={createActivity}
-              editActivity={editActivity}
-            />
-          )}
-        </Grid.Column>
-      </Grid>
-    );
-  };
-  
-  export default ActivityDashboard;
-  
+        )}
+        {editMode && (
+          <ActivityForm
+            key={(selectedActivity && selectedActivity.id) || 0}
+            setEditMode={setEditMode}
+            activity={selectedActivity!}
+            createActivity={createActivity}
+            editActivity={editActivity}
+            submitting={submitting}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default ActivityDashboard;
